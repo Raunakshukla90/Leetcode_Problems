75b0eb6230232114f96d5dde1db5b44905b88193
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-03 19:20:13 +0530

    Time: 8 ms (54.46%), Space: 22.9 MB (53.38%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+     bool check(int speed,vector<int>&piles,int h){
+        int count=0;
+        int n=piles.size();
+        for(int i=0;i<n;i++){if(count>h) return false;
+        if(speed>=piles[i])count++;
+        else if(piles[i]%speed ==0) count+=piles[i]/speed;
+        else count+=piles[i]/speed+1 ;
+     }
+     if(count>h)return false;
+     else return true;
+     }
+    int minEatingSpeed(vector<int>& piles, int h) {
+       int n=piles.size();
+       int mx=-1;
+       for(int i=0;i<n;i++){
+        mx=max(mx,piles[i]);
+       }
+       int lo=1;
+       int hi=mx;
+       int ans=-1;
+       while(lo<=hi){
+        int mid=(lo+hi)/2;
+        if(check(mid,piles,h)==true)
+         {
+            ans=mid;
+            hi=mid-1;
+         }      
+         else lo=mid+1;
+      }
+      return ans;
+
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
