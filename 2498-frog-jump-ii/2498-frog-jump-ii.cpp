3f24a030b03dbce3edf78b5aaa887fd569cafd48
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-04 16:21:21 +0530

    Time: 0 ms (100%), Space: 69.1 MB (51.7%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+        int n=stones.size();
+       
+        if(n==2){
+             return stones[1]-stones[0]; 
+        }
+        int ans=0;
+        for(int i=2;i<n;i++) {
+            ans=max(stones[i]-stones[i-2],ans);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
