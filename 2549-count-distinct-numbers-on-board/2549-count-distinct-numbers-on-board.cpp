fb116ba1ddcf26994cb2c8a0fa5a4caab1762153
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-23 23:20:28 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.86%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int distinctIntegers(int n){
+
+     return n == 1 ? 1 : n - 1;   
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
