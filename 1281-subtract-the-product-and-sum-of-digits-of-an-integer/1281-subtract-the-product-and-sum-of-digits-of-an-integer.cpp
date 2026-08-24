439d6b2fc44abbbf817fdbd38aa7fb38bb4b230a
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-24 22:05:22 +0530

    Time: 0 ms (100%), Space: 8 MB (20.72%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int lastdigit;
+        int product=1;
+        int sum=0;
+        while(n>0){
+            lastdigit=n%10;
+            sum=sum+lastdigit;
+            product=product*lastdigit;
+            n/=10;
+        }
+        return product-sum;
+
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
