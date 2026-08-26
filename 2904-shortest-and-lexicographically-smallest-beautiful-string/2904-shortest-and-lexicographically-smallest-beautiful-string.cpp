b9ab0a644290cbbf52e4a3f0902a80e1560d969e
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-26 23:44:04 +0530

    Time: 2 ms (34.51%), Space: 9.3 MB (30.48%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        string ans = "";
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+
+            int oneCnt = 0;
+            string cur = "";
+
+            for (int j = i; j < n; j++) {
+
+                cur += s[j];
+
+                if (s[j] == '1')
+                    oneCnt++;
+
+                // More than k ones can never become valid again
+                if (oneCnt > k)
+                    break;
+
+                if (oneCnt == k) {
+
+                    if (ans == "" ||
+                        cur.size() < ans.size() ||
+                        (cur.size() == ans.size() && cur < ans)) {
+
+                        ans = cur;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
