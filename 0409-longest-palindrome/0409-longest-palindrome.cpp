add9c997e5c515679ac0aa50dcc02d3e0e26c8d4
Raunak Shukla <raunakshukla5861@gmail.com>
2026-09-07 22:08:58 +0530

    Time: 1 ms (25.97%), Space: 9.4 MB (41.17%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int oddCount = 0;
+        unordered_map<char, int> ump;
+        for(char ch : s) {
+            ump[ch]++;
+            if (ump[ch] % 2 == 1)
+                oddCount++;
+            else    
+                oddCount--;
+        }
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
