fb43eb63db3117b6e1fd42141fdcb46dc82bf97e
Raunak Shukla <raunakshukla5861@gmail.com>
2026-09-17 21:51:00 +0530

    Time: 0 ms (100%), Space: 8.7 MB (67.76%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long left = 1, right = n;
+        while(left <= right){
+            long mid = left + (right - left) / 2;
+            long coins_needed = mid * (mid + 1) / 2;
+            if(coins_needed == n) return mid;
+            else if(coins_needed < n) left = mid + 1;
+            else right = mid - 1;
+        }
+        return right;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
