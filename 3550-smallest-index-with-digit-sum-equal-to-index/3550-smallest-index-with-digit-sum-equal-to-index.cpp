15d54a5b51521ec92e8b45d27082ef5e07f9de1e
Raunak Shukla <raunakshukla5861@gmail.com>
2026-09-24 22:32:52 +0530

    Time: 0 ms (100%), Space: 30.9 MB (52.36%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+         for(int i = 0; i<nums.size(); i++){
+            int x = nums[i];
+            int sum = 0;
+            for (int t = nums[i]; t > 0; t /= 10) sum += t % 10;
+            if(sum==i) return i;
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
